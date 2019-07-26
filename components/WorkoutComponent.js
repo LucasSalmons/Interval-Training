02762d2c589114f0f297d9681a367d8e85c9e107
Lucas Salmons <lucsalmons@gmail.com>
2019-07-25 21:55:34 -0700

    countdown timer

--- components/WorkoutComponent.js
+++ components/WorkoutComponent.js
@@ -1,31 +1,53 @@
 import React, { Component } from 'react';
 import { View, ScrollView, Text, Image, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
+import CountDown from 'react-native-countdown-component';
 import { SPARTACUS } from '../shared/spartacus';
 
-const RenderWorkout = (props) => {
-    const spartacus = props.spartacus;
+class RenderWorkout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            interval: 0
+        }
+    }
 
-    const renderWorkoutScene = ({ item, index }) => {
+    countDownOver() {
+        this.setState({
+            interval: this.state.interval + 1
+        })
+    }
+
+    render() {
+        const spartacus = this.props.spartacus;
+        let activeSpartacus = spartacus.filter((spartacus) => spartacus.id === this.state.interval);
+
+        console.log(activeSpartacus);
+        const renderWorkoutScene = ({ item, index }) => {
+            return (
+                <View key={index} style={{ margin: 20 }}>
+                    <Text style={{ fontSize: 20 }}>{item.name}</Text>
+                    <Text style={{ fontSize: 14 }}>{item.description}</Text>
+                    <Image style={{ width: 200, height: 200 }} source={require('./images/bunny.png')} />
+                    <Image style={{ width: 200, height: 200 }} source={require('./images/bunny.png')} />
+                </View>
+            );
+        }
         return (
-            <View key={index} style={{ margin: 20 }}>
-                <Text style={{ fontSize: 20 }}>{item.name}</Text>
-                <Text style={{ fontSize: 14 }}>{item.description}</Text>
-                <Image style={{ width: 200, height: 200 }} source={require('./images/bunny.png')} />
-                <Image style={{ width: 200, height: 200 }} source={require('./images/bunny.png')} />
-            </View>
+            <Card>
+                <CountDown
+                    size={20}
+                    timeToShow={['M', 'S']}
+                    until={activeSpartacus.seconds}
+                    onFinish={() => this.countDownOver()} />
+                <FlatList
+                    data={activeSpartacus}
+                    renderItem={renderWorkoutScene}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Card>
         );
-    };
-
-    return (
-        <Card title='Excercises'>
-            <FlatList
-                data={spartacus}
-                renderItem={renderWorkoutScene}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
-    )
+    }
 }
 
 class Workout extends Component {
@@ -39,6 +61,7 @@ class Workout extends Component {
     render() {
         return (
             <ScrollView>
+                <Text>Timer Here</Text>
                 <RenderWorkout spartacus={this.state.spartacus} />
             </ScrollView>
         );
--- shared/spartacus.js
+++ shared/spartacus.js
@@ -5,69 +5,79 @@ export const SPARTACUS =
             name: "Goblet Squat",
             description: "With both hands, grab one end of a dumbbell to hold it vertically in front of your chest, and stand with your feet slightly beyond shoulder width [A]. Keeping your back naturally arched, push your hips back, bend your knees, and lower your body until the tops of your thighs are at least parallel to the floor [B]. Pause, and push yourself up to the starting position. If that's too hard, do a body-weight squat instead.",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         },
         {
             id: 1,
             name: "Mountain Climber",
             description: "Assume a pushup position with your arms completely straight. Your body should form a straight line from your head to your ankles [A]. Without allowing your lower-back posture to change, lift your right foot off the floor and slowly move your right knee toward your chest [B]. Return to the starting position, and repeat with your left leg. Alternate back and forth for the duration of the set.",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         },
         {
             id: 2,
             name: "Single-Arm Dumbbell Swing",
             description: "Hold a dumbbell (or a kettlebell) at arm's length in front of your waist. Without rounding your lower back, bend at your hips and knees and swing the dumbbell between your legs [A]. Keeping your arm straight, thrust your hips forward and swing the dumbbell to shoulder level as you rise to a standing position [B]. Swing the weight back and forth. At the 30-second mark, switch arms.",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         },
         {
             id: 3,
             name: "T-Pushup",
             description: "Grab a pair of hex dumbbells and assume a pushup position, your arms straight [A]. Bend your elbows and lower your body until your chest nearly touches the floor [B]. As you push yourself back up, lift your right hand and rotate the right side of your body as you raise the dumbbell straight up over your shoulder until your body forms a T [C]. Reverse the move and repeat, this time rotating your left side. ",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         },
         {
             id: 4,
             name: "Split Jump",
             description: "Stand in a staggered stance with your feet 2 to 3 feet apart, your right foot in front of your left. Keeping your torso upright, bend your legs and lower your body into a lunge [A]. Now jump with enough force to propel both feet off the floor [B]. While you're in the air, scissor-kick your legs so you land with your left leg forward. Repeat, alternating your forward leg for the duration of the set.",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         },
         {
             id: 5,
             name: "Dumbbell Row",
             description: "Grab a pair of dumbbells, bend at your hips (don't round your lower back), and lower your torso until it's nearly parallel to the floor. Let the dumbbells hang at arm's length [A]. Without moving your torso, row the weights upward by raising your upper arms, bending your elbows, and squeezing your shoulder blades together [B]. Pause, lower the dumbbells, and repeat.",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         },
         {
             id: 6,
             name: "Dumbbell Side Lunge and Touch",
             description: "Hold a pair of dumbbells at arm's length at your sides [A]. Take a big step to your left and lower your body by pushing your hips backward and bending your left knee. As you lower your body, bend forward at your hips and touch the dumbbells to the floor [B]. Repeat for 30 seconds, and then switch to your right leg. If the exercise is too hard, do the move without the dumbbells; just reach for the floor with your hands.",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         },
         {
             id: 7,
             name: "Pushup-Position Row",
             description: "Grab a pair of hex dumbbells and assume a pushup position, your arms straight [A]. Keeping your core stiff, row the dumbbell in your right hand to the side of your chest, bending your arm as you pull it upward [B]. Pause, and then quickly lower the dumbbell. Repeat with your left arm.",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         },
         {
             id: 8,
             name: "Dumbbell Lunge and Rotation",
             description: "Grab a dumbbell and hold it horizontally by its ends, just under your chin [A]. Step forward with your right foot and lower your body into a lunge. As you lunge, rotate your upper body to the right [B]. Return to the starting position, and repeat with your left leg. Alternate left and right until your 60 seconds are up. If the exercise is too hard, perform the movement without the dumbbell.",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         },
         {
             id: 9,
             name: "Dumbbell Push Press",
             description: "Stand holding a pair of dumbbells just outside your shoulders, with your arms bent and palms facing each other. Stand with your feet shoulder-width apart and knees slightly bent [A]. Dip your knees [B], and then explosively push up with your legs as you press the weights straight over your shoulders [C]. Lower the dumbbells back to the starting position and repeat.",
             positionA: "",
-            positionB: ""
+            positionB: "",
+            seconds: 60,
         }
     ]
